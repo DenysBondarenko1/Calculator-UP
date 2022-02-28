734ec8773177adba097df407a688cc503c1f1a1d
DenysBondarenko <DenysBondarenko@S24K02>
2022-02-28 12:03:22 +0100

    Dodaj pliki projektów.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void liczba_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void przecinek_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void wynik_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void systemBinarny_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void czyszczenie_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void silnia_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dodawanie_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void potęga_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void odejmowanie_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dzielenie_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnożenie_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+ 
